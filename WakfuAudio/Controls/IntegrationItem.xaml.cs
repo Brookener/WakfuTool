376d2ed44d03eb783b0027c26c7fe61f31eafa6a
Brookener <46428517+Brookener@users.noreply.github.com>
2021-12-10 17:09:10 +0100

    Recorrection

--- WakfuAudio/Controls/IntegrationItem.xaml.cs
+++ WakfuAudio/Controls/IntegrationItem.xaml.cs
@@ -108,7 +108,7 @@ namespace WakfuAudio
             if (Keyboard.IsKeyDown(Key.LeftShift))
                 MainWindow.ShowFileInExplorer(inte.AssetFile());
             else
-                inte.PlayAssetSource(1);
+                inte.PlayAssetSource();
         }
         private void CopyAssetClick(object sender, RoutedEventArgs e)
         {
